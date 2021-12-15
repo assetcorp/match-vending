c9793cf023ffee2970652d04a69e17f540461ea0
Delali <michaelazumah@live.com>
2021-12-15 01:01:01 +0000

    complete user handler

---
+++ server/handles/user.handler.js
@@ -0,0 +1,154 @@
+import {
+	getAllUsers, getOneUser, patchUserDeposit, patchUserDepositReset,
+	patchUserRole, removeUser, restoreUserFromDeleted, userLogin, userSignUp,
+} from '../controllers/user.controller'
+import { buildResponse, genericErrorMessage, validateJwt, validateRequest } from '../utils'
+
+export const signUpUser = async ( req, res ) => {
+	try {
+		const requiredBody = ['username', 'password']
+		if ( validateRequest( req, res, requiredBody, 'body' ) ) {
+			const { username, password } = req.body
+
+			const newUser = await userSignUp( username, password )
+			if ( newUser.error ) throw newUser
+
+			return res.status( newUser.status )
+				.send( buildResponse( newUser.message, {
+					token: newUser.token
+				} ) )
+		}
+	} catch ( error ) {
+		return res
+			.status( error.status || 500 )
+			.send( buildResponse( error.message || genericErrorMessage ) )
+	}
+}
+
+export const loginUser = async ( req, res ) => {
+	try {
+		const requiredBody = ['username', 'password']
+		if ( validateRequest( req, res, requiredBody, 'body' ) ) {
+			const { username, password } = req.body
+
+			const user = await userLogin( username, password )
+			if ( user.error ) throw user
+
+			return res
+				.status( user.status )
+				.send( buildResponse( user.message, { token: user.token } ) )
+		}
+	} catch ( error ) {
+		return res
+			.status( error.status || 500 )
+			.send( buildResponse( error.message || genericErrorMessage ) )
+	}
+}
+
+export const allUsers = async ( req, res ) => {
+	try {
+		if ( validateJwt( req, res ) ) {
+			const { limit, offset } = req.query
+
+			const allUsers = await getAllUsers( limit, offset )
+			if ( allUsers.error ) throw new Error( allUsers )
+
+			return res
+				.status( allUsers.status )
+				.send( buildResponse( allUsers.message, allUsers.users ) )
+		}
+	} catch ( error ) {
+		return res
+			.status( error.status || 500 )
+			.send( buildResponse( error.message || genericErrorMessage ) )
+	}
+}
+
+export const singleUser = async ( req, res ) => {
+	try {
+		if ( validateJwt( req, res ) ) {
+			const username = req.jwt.username
+
+			const user = await getOneUser( username )
+			if ( user.error ) throw new Error( user )
+
+			return res
+				.status( user.status )
+				.send( buildResponse( user.message, user.data ) )
+		}
+	} catch ( error ) {
+		return res
+			.status( error.status || 500 )
+			.send( buildResponse( error.message || genericErrorMessage ) )
+	}
+}
+
+export const patchUpUser = async ( req, res ) => {
+	try {
+		if ( validateJwt( req, res ) ) {
+			const username = req.jwt.username
+
+			if ( 'deposit' in req.body ) {
+				const newUser = await patchUserDeposit( username, req.body.deposit )
+				if ( newUser.error ) throw new Error( newUser )
+			}
+			if ( 'depositReset' in req.body ) {
+				const newUser = await patchUserDepositReset( username )
+				if ( newUser.error ) throw new Error( newUser )
+			}
+			if ( 'role' in req.body ) {
+				const newUser = await patchUserRole( username, req.body.role )
+				if ( newUser.error ) throw new Error( newUser )
+			}
+
+			const user = await getOneUser( username )
+			if ( user.error ) throw new Error( user )
+
+			return res
+				.status( user.status )
+				.send( buildResponse( user.message, user.data ) )
+		}
+	} catch ( error ) {
+		return res
+			.status( error.status || 500 )
+			.send( buildResponse( error.message || genericErrorMessage ) )
+	}
+}
+
+export const deleteUser = async ( req, res ) => {
+	try {
+		if ( validateJwt( req, res ) ) {
+			const username = req.jwt.username
+
+			const user = await removeUser( username )
+			if ( user.error ) throw new Error( user )
+
+			return res
+				.status( user.status )
+				.send( buildResponse( user.message, user.data ) )
+		}
+	} catch ( error ) {
+		return res
+			.status( error.status || 500 )
+			.send( buildResponse( error.message || genericErrorMessage ) )
+	}
+}
+
+export const restoreUser = async ( req, res ) => {
+	try {
+		if ( validateJwt( req, res ) ) {
+			const username = req.jwt.username
+
+			const user = await restoreUserFromDeleted( username )
+			if ( user.error ) throw new Error( user )
+
+			return res
+				.status( user.status )
+				.send( buildResponse( user.message, user.data ) )
+		}
+	} catch ( error ) {
+		return res
+			.status( error.status || 500 )
+			.send( buildResponse( error.message || genericErrorMessage ) )
+	}
+}
