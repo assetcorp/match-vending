691b601a347500c5c3b1ecd70e10d592593c8488
Delali <michaelazumah@live.com>
2021-12-15 11:29:33 +0000

    define user models

--- server/models/index.js
+++ server/models/index.js
@@ -1,24 +1,49 @@
-import databaseConfig from '../config/db.config'
-import { Sequelize } from 'sequelize'
+const databaseConfig = require( '../config/db.config' )
+const Sequelize = require( 'sequelize' )
+const fs = require( 'fs' )
+const path = require( 'path' )
 
 // Database connection via sequelize
 const sequelizeConnection = new Sequelize(
-  databaseConfig.DB,
-  databaseConfig.USER,
-  databaseConfig.PASSWORD, {
-  host: databaseConfig.HOST,
-  dialect: databaseConfig.dialect,
-  pool: {
-    min: databaseConfig.pool.min,
-    max: databaseConfig.pool.max,
-    acquire: databaseConfig.pool.acquire,
-    idle: databaseConfig.pool.idle,
-  }
+	databaseConfig.DB,
+	databaseConfig.USER,
+	databaseConfig.PASSWORD, {
+	host: databaseConfig.HOST,
+	dialect: databaseConfig.dialect,
+	pool: {
+		min: databaseConfig.pool.min,
+		max: databaseConfig.pool.max,
+		acquire: databaseConfig.pool.acquire,
+		idle: databaseConfig.pool.idle,
+	},
+	define: {
+		underscored: false,
+		charset: 'utf8mb4',
+		collate: 'utf8mb4_unicode_ci',
+		timestamps: true
+	}
 } )
 
-// Create a database object passing in the sequelize object and the connection
-const database = {}
-database.Sequelize = Sequelize
-database.connection = sequelizeConnection
+const basename = path.basename( __filename )
+const db = {}
+fs
+	.readdirSync( __dirname )
+	.filter( file => {
+		return ( file.indexOf( '.' ) !== 0 ) && ( file !== basename )
+	} )
+	.forEach( file => {
+		const model = require( path.join( __dirname, file ) )
+		db[model.name] = model
+	} )
 
-export default database
+Object.keys( db ).forEach( modelName => {
+	if ( db[modelName].associate ) {
+		db[modelName].associate( db )
+	}
+} )
+
+db.sequelize = sequelizeConnection
+db.Sequelize = Sequelize
+
+module.exports = db
+
